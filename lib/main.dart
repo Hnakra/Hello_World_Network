35af39d27aec0e99788ce12505dd49367bed001a
Ivan <VanekMRZ@yandex.ru>
2022-07-15 19:16:18 +0300

    Реализация подключения к удаленному серверу, получение данных, обновление состояния виджета

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -48,16 +49,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String? _date;
 
-  void _incrementCounter() {
+  // конструктор для инициализации значения
+  _MyHomePageState(){
+    _getData();
+  }
+  // приватный ассихронный метод получения текущего московского времени
+  void _getData() async{
+    var dio = Dio();
+    final response = await dio.get('http://worldtimeapi.org/api/timezone/Europe/Moscow');
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _date = response.data['datetime'];
     });
   }
 
@@ -96,19 +99,20 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Текущее время:',
             ),
+
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '$_date',
+              style: const TextStyle(fontSize: 16),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _getData,
+        tooltip: 'Update date',
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
